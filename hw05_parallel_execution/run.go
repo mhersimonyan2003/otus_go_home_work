e20ff6c8dcb8ebbacaa0f700476e55ff7cb8fa26
mhersimonyan2003 <mher.simonyan.2014@gmail.com>
2024-03-19 23:13:40 +0400

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -6,49 +6,78 @@ import (
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
-var wg = sync.WaitGroup{}
-var sm = sync.Mutex{}
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+func controller(doneChan chan struct{}, tasks []Task, m int) (chan Task, chan struct{}, *int) {
+	tasksChan := make(chan Task)
+	errorsChan := make(chan struct{})
+
+	count := 0
+	errors := 0
+
+	go func() {
+		defer close(doneChan)
+
+		for {
+			select {
+			case tasksChan <- tasks[count]:
+				count++
+
+				if count == len(tasks) {
+					return
+				}
+			case <-errorsChan:
+				errors++
+
+				if errors == m {
+					return
+				}
+			}
+		}
+	}()
+
+	return tasksChan, errorsChan, &errors
+}
+
+func consumer(doneChan chan struct{}, wg *sync.WaitGroup, tasksChan chan Task, errorsChan chan struct{}) {
+	defer wg.Done()
+
+	for {
+		select {
+		case <-doneChan:
+			return
+		case task := <-tasksChan:
+			err := task()
+			if err != nil {
+				select {
+				case <-doneChan:
+					return
+				case errorsChan <- struct{}{}:
+				}
+			}
+		}
+	}
+}
+
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
 	if m <= 0 {
 		return ErrErrorsLimitExceeded
 	}
 
-	count := 0
+	wg := sync.WaitGroup{}
+	doneChan := make(chan struct{})
+
+	tasksChan, errorsChan, errors := controller(doneChan, tasks, m)
 
 	for i := 0; i < n; i++ {
 		wg.Add(1)
-		go func() {
-			defer wg.Done()
-
-			for count < len(tasks) {
-				if m <= 0 {
-					return
-				}
-
-				sm.Lock()
-				task := tasks[count]
-				count++
-				sm.Unlock()
-
-				err := task()
-
-				sm.Lock()
-				if err != nil {
-					m--
-				}
-				sm.Unlock()
-			}
-		}()
+		go consumer(doneChan, &wg, tasksChan, errorsChan)
 	}
 
 	wg.Wait()
 
-	if m <= 0 {
+	if *errors >= m {
 		return ErrErrorsLimitExceeded
 	}
 
