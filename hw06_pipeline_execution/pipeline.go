0efb4551ca4cbebfa0feefc6537ff7009cc1406e
mhersimonyan2003 <mher.simonyan.2014@gmail.com>
2024-04-17 15:18:54 +0400

    HW6 fixes

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,69 +1,34 @@
 package hw06pipelineexecution
 
-import (
-	"sync"
-)
-
 type (
 	In  = <-chan interface{}
 	Out = In
 	Bi  = chan interface{}
 )
 
-var (
-	wg = sync.WaitGroup{}
-	sm = sync.Mutex{}
-)
-
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	result := make(chan interface{})
-	values := make([]interface{}, 0)
-	index := 0
 
-	for value := range in {
-		values = append(values, nil)
-		stageInChan := make(chan interface{})
-		wg.Add(1)
+	for _, stage := range stages {
+		inChan := make(Bi)
 
-		go func(index int) {
-			defer wg.Done()
-			var stageOutChan In
-			stageOutChan = stageInChan
+		go func(in In) {
+			defer close(inChan)
 
-			for _, stage := range stages {
-				stageOutChan = stage(stageOutChan)
+			for value := range in {
+				select {
+				case <-done:
+					return
+				default:
+				}
+
+				inChan <- value
 			}
+		}(in)
 
-			select {
-			case stageValue := <-stageOutChan:
-				sm.Lock()
-				values[index] = stageValue
-				sm.Unlock()
-			case <-done:
-				sm.Lock()
-				values = nil
-				sm.Unlock()
-				return
-			}
-		}(index)
-
-		stageInChan <- value
-		sm.Lock()
-		index++
-		sm.Unlock()
+		in = stage(inChan)
 	}
 
-	wg.Wait()
-
-	go func() {
-		defer close(result)
-
-		for _, value := range values {
-			result <- value
-		}
-	}()
-
-	return result
+	return in
 }
